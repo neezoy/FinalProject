ac23b64bf6f32c86184feb6d1871faa6966f9566
Airo <valdemar_hh@hotmail.com>
2021-04-19 23:11:13 +0200

    Server XML working

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,32 +1,103 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Client
 {
+
+    public class MyMessage
+    {
+        public string StringProperty { get; set; }
+        public int IntProperty { get; set; }
+    }
+
     class Client
     {
 
 
+       
+
+
         static async Task Send<T>(NetworkStream stream, T message)
         {
+            var (header, body) = Encode(message);
+            await stream.WriteAsync(header, 0, header.Length).ConfigureAwait(false);
+            await stream.WriteAsync(body, 0, body.Length);
+
 
 
         }
 
-        static async Task Receive<T>(NetworkStream stream)
+        static async Task<T> Receive<T>(NetworkStream stream)
         {
+            var header = await ReadAsync(stream, 4); //only read 4 first bytes
+            //Header bytes contain body length.
+            var bodyLength = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(header)); //changing bytes from network order to Host order. 
 
+            var bodyBytes = await ReadAsync(stream, bodyLength);
+
+            return Decode<T>(bodyBytes);
         }
 
         static (byte[] header, byte[] body) Encode<T>(T message)
         {
+            XmlSerializer xml = new XmlSerializer(typeof(T));
+            StringBuilder stringBuilder = new StringBuilder();
+            StringWriter stringWriter = new StringWriter(stringBuilder);
+
+            //Serialize xml message to string with stringWriter and stores it in the stringbuilder.
+            xml.Serialize(stringWriter, message);
+
+
+            var bodyBytes = System.Text.Encoding.UTF8.GetBytes(stringBuilder.ToString());
+
+            //calculates the length of body and creates header
+            var headerBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(bodyBytes.Length));
+
+            return (headerBytes, bodyBytes);
 
         }
 
+        static T Decode<T>(byte[] body)
+        {
+            var s = System.Text.Encoding.UTF8.GetString(body);
 
-        static void Main(string[] args)
+            //creating reader for xml deserializer
+            StringReader stringReader = new StringReader(s);
+
+            XmlSerializer xml = new XmlSerializer(typeof(T));
+
+            //deserialize from stringto XML
+            return (T)xml.Deserialize(stringReader);
+
+
+        }
+
+        static async Task<byte[]> ReadAsync(NetworkStream stream, int bytesToRead)
+        {
+            var buffer = new byte[bytesToRead];
+            var bytesRead = 0;
+            while(bytesRead < bytesToRead)
+            {
+                //runs in seperate thread, so I wont return it untill all bytes have been read.
+                var bytesReceived = bytesRead + await stream.ReadAsync(buffer, bytesRead, (bytesToRead - bytesRead)).ConfigureAwait(false);            
+                
+                if (bytesReceived == 0)
+                {
+                    throw new Exception("Socket Closed");
+                }
+                bytesRead = bytesRead + bytesReceived;
+            }
+
+            return buffer;
+        }
+
+
+        static async Task Main(string[] args)
         {
 
             Console.WriteLine("Press Enter to Connect");
@@ -42,22 +113,28 @@ namespace Client
 
             NetworkStream stream = new NetworkStream(socket, true);
 
-            String stringMessage = "Test";
-            
-            //send
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(stringMessage);
-            stream.Write(buffer, 0, buffer.Length); // buffer, offset, length
-            Console.WriteLine("Received: " + stringMessage);
+            var myMessage = new MyMessage
+            {
+                IntProperty = 404,
+                StringProperty = "Hello World"
+            };
 
-            //receive
-            byte[] response = new byte[1024];
-            stream.Read(response, 0, response.Length);
-            String stringResponse = System.Text.Encoding.UTF8.GetString(response);
 
-            //display response
-            Console.WriteLine("Received: " + stringResponse);
+            Console.WriteLine("Sending");
+            Print(myMessage);
+
+            await Send(stream, myMessage).ConfigureAwait(false);
+
+            var responseMsg = await Receive<MyMessage>(stream).ConfigureAwait(false);
+
+            Console.WriteLine("Received");
+            Print(responseMsg);
+
+
             Console.ReadLine();
 
+
         }
+        static void Print(MyMessage m) => Console.WriteLine($"MyMessage.IntProperty = {m.IntProperty}, MyMessage.StringProperty = {m.StringProperty}");
     }
 }
